a2e221642513d1c3b82e69cb53d7c6a3f41818bb
gsonkow <sonkowskyg@gmail.com>
2023-09-07 21:12:07 -0700

    RollaBall tutorial

--- RollaBall/Assets/Scripts/PlayerController.cs
+++ RollaBall/Assets/Scripts/PlayerController.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
+    private int count;
     private Rigidbody rb;
     private float movementX;
     private float movementY;
@@ -15,6 +19,9 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -25,9 +32,28 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
     }
 }
